fe760f1d523b4c5b1f8d2c137204c29cab181b74
Josue Almanza Martinez <kelly.almanza.mtz@gmail.com>
2025-05-11 14:56:14 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mensajesfirebase/FirebaseMessagingService.kt
@@ -0,0 +1,18 @@
+package com.example.mensajesfirebase
+
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.firebaseconcloud.MainActivity
+import com.example.firebaseconcloud.R
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FirebaseMessagingService : FirebaseMessagingService() {
+
+   
+
+}
